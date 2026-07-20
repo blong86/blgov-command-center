afb1c779747901f251d7b56b7e261e15a364edec
blong86 <brian@blgovsolutions.com>
2026-07-20 14:32:55 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,35 @@
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+const CACHE_NAME = "blgov-cc-v6";
+const ASSETS = [
+  "/blgov-command-center/BLGov_Command_Center.html",
+  "/blgov-command-center/manifest.json",
+  "/blgov-command-center/icon.png",
+  "/blgov-command-center/icon512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(()=>{});
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
